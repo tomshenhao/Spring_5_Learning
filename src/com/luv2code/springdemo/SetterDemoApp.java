321a7c1de5db06db0ffaa63818ae3855db09dc13
t822660 <t822660@spark.co.nz>
2019-09-27 13:02:14 +1200

    new files

--- src/com/luv2code/springdemo/SetterDemoApp.java
+++ src/com/luv2code/springdemo/SetterDemoApp.java
@@ -15,8 +15,11 @@ public class SetterDemoApp {
 				
 		// call methods on the bean
 		System.out.println(theCoach.getDailyFortune());
-
 		System.out.println(theCoach.getDailyWorkout());
+		
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
+		
 		// close the context
 		context.close();
 	}
--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -18,4 +18,14 @@ public class TrackCoach implements Coach {
 		return "Just Do It "+fortuneService.getFortune();
 	}
 	
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+	
+	// add a destory method
+	public void doMyCleanupStuff() {
+		System.out.println("TrackCoach: inside method doMyCleanupStuff");
+	}
+	
 }
